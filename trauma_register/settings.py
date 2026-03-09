5bfa71631de8a08a47ad4c65734d468bac9194c8
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2026-03-09 16:15:07 -0500

    chore: actualización  a CORS

--- trauma_register/settings.py
+++ trauma_register/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",  # Cambia esto por la URL de tu frontend en producción
+    "http://localhost:8080",  # Cambia esto por la URL de tu frontend en producción
     "http://127.0.0.1:8000",
     "http://localhost:3000",  # Si usas otro puerto en desarrollo
     f"http://localhost:{variable_frontend_port}", # Para el puerto variable en Flutter
